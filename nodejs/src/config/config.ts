6f9d6b7d8934b08b85832211f895cf3fb9631729
Mere Soares <36089437+meresoares@users.noreply.github.com>
2024-04-28 18:55:15 -0400

    Update config.ts

--- nodejs/src/config/config.ts
+++ nodejs/src/config/config.ts
@@ -13,8 +13,8 @@ dotenv.config();
  */
 const sequelize = new Sequelize(
   process.env.DB_NAME || 'Persona',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASS || 'admin', {
+  process.env.DB_USER || '',
+  process.env.DB_PASS || '', {
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
   dialect: 'postgres',
